abd76cf4f582aaa218c6e97f333995f639f55545
kazimhah <kazimh.ah@gmail.com>
2016-03-10 10:44:23 -0800

    Second commit

--- src/main/java/com/sampleservice/rest/Hello.java
+++ src/main/java/com/sampleservice/rest/Hello.java
@@ -26,7 +26,7 @@ public class Hello {
   @Path("/{param}")
   public Response getMsg(@PathParam("param") String msg) {
  
-    String output = "Asad & Zakeer say Asalaam Alikum to  :Mr " + msg;
+    String output = "Welcome to DevOps training : Mr. " + msg;
  
     return Response.status(200).entity(output).build();
  
